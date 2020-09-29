33d9b79c31953289beab843b2a8908b4bcc660cf
Prem Saini <44334301+prem-saini@users.noreply.github.com>
2020-09-29 16:29:41 +0530

    Create park.py

---
+++ park.py
@@ -0,0 +1,25 @@
+# If-Else-condition-With-park-entry-program
+
+age=int(input("Enter your age in years: "))
+print(f"\nyour age:{age}")
+if(age<=0):
+    print("littel baby")
+elif(age>=1 and age<=10):
+    print("fun only")
+elif(age>11 and age<=18):
+    print("not Masti")
+elif(age>19 and age<=30):
+    print("your age only romance")
+elif(age>31 and age<=50):
+    print("work only work")
+elif(age>51 and age<=80):
+    print("take rest")
+elif(age>80 and age<=90):
+    print("bad rest")
+elif(age>90):
+    print("wait for yamraj")
+else:
+    print("invaild entery")
+
+                                    
+
